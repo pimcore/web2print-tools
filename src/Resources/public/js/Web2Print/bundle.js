97ec14b04b2944962366030dbc1ca095fadcd0e0
Bernhard Rusch <bernhard.rusch@elements.at>
2019-05-15 16:58:16 +0200

    [UI] moved favorite output config into settings menu

--- src/Resources/public/js/Web2Print/bundle.js
+++ src/Resources/public/js/Web2Print/bundle.js
@@ -28,53 +28,23 @@ pimcore.bundle.web2print = Class.create(pimcore.plugin.admin, {
 
     pimcoreReady: function (params, broker) {
         var user = pimcore.globalmanager.get("user");
-        var toolbar = pimcore.globalmanager.get("layout_toolbar");
         var perspectiveCfg = pimcore.globalmanager.get("perspective");
 
-        if (perspectiveCfg.inToolbar("web2print")) {
-            // init
-            var menuItems = toolbar.web2printMenu;
-            if (!menuItems) {
-                menuItems = new Ext.menu.Menu({cls: "pimcore_navigation_flyout"});
-                toolbar.web2printMenu = menuItems;
-            }
 
-            if (perspectiveCfg.inToolbar("web2print.favorite_outputdefinitions") && user.isAllowed("web2print_web2print_favourite_output_channels")) {
-                menuItems.add({
-                    text: t("web2print_favorite_outputdefinitions"),
-                    iconCls: "bundle_outputdataconfig_icon bundle_outputdataconfig_icon_overlay",
-                    cls: "pimcore_main_menu",
-                    handler: function () {
-                        try {
-                            pimcore.globalmanager.get("web2print.favorite_outputdefinitions").activate();
-                        }
-                        catch (e) {
-                            pimcore.globalmanager.add("web2print.favorite_outputdefinitions", new pimcore.bundle.web2print.favoriteOutputDefinitionsTable());
-                        }
+        if (user.isAllowed("web2print_web2print_favourite_output_channels") && perspectiveCfg.inToolbar("settings.favorite_outputdefinitions")) {
+            var menu = pimcore.globalmanager.get("layout_toolbar").settingsMenu;
+            menu.add({
+                text: t("web2print_favorite_outputdefinitions"),
+                iconCls: "bundle_outputdataconfig_icon bundle_outputdataconfig_icon_overlay",
+                handler: function () {
+                    try {
+                        pimcore.globalmanager.get("web2print.favorite_outputdefinitions").activate();
                     }
-                });
-            }
-
-            if (menuItems.items.length > 0) {
-
-                var insertPoint = Ext.get("pimcore_menu_settings");
-                if (!insertPoint) {
-                    var dom = Ext.dom.Query.select('#pimcore_navigation ul li:last');
-                    insertPoint = Ext.get(dom[0]);
+                    catch (e) {
+                        pimcore.globalmanager.add("web2print.favorite_outputdefinitions", new pimcore.bundle.web2print.favoriteOutputDefinitionsTable());
+                    }
                 }
-
-                this.navEl = Ext.get("pimcore_menu_web2print");
-                if (!this.navEl) {
-                    this.navEl = Ext.get(
-                        insertPoint.insertHtml(
-                            "afterEnd",
-                            '<li id="pimcore_menu_web2print" class="pimcore_menu_item icon-print compatibility">' + t('web2print_web2print_mainmenu') + '</li>'
-                        )
-                    );
-                }
-
-                this.navEl.on("mousedown", toolbar.showSubMenu.bind(menuItems));
-            }
+            });
         }
     }
 });

6b86cc060dc9f9d25b31a9b58890e660b8c7feb9
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2022-12-19 09:34:36 +0100

    [Bug]: Can't add Output Channel Configuration (#50)
    * Fix: Adding an Output Channel Config
    
    * Apply php-cs-fixer changes
    
    Co-authored-by: robertSt7 <robertSt7@users.noreply.github.com>

--- src/FavoriteOutputDefinition/Dao.php
+++ src/FavoriteOutputDefinition/Dao.php
@@ -74,9 +74,9 @@ class Dao extends \Pimcore\Model\Dao\AbstractDao
     {
         if ($this->model->getId()) {
             $this->update();
+        } else {
+            $this->create();
         }
-
-        $this->create();
     }
 
     /**

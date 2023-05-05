603a0194dd2bf96b9a34064fbeb8de2caba4584c
JiaJia Ji <kingjia90@gmail.com>
2023-05-05 14:58:29 +0200

    task: rebase

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -116,7 +116,7 @@ class AdminController extends \Pimcore\Bundle\AdminBundle\Controller\AdminContro
         $list = new FavoriteOutputDefinition\Listing();
         $list->setOrder('asc');
         $list->setOrderKey('description');
-        $condition = 'o_classId = ' . $request->get('classId');
+        $condition = 'o_classId = ' . $list->quote($request->get('classId'));
         $list->setCondition($condition);
 
         $definitions = [];

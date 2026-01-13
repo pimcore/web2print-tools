7714452a04b9f9b077752784af4b8d0b05e464a1
JiaJia Ji <kingjia90@gmail.com>
2026-01-13 09:20:56 +0100

    [Bug]: Add permission checks in AdminController actions (#108)
    * Add permission checks in AdminController actions
    
    * fix typo
    
    * it seems intended that these 2 are always allowed
    
    Removed permission check from favorite output definitions actions.

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -38,6 +38,8 @@ class AdminController extends UserAwareController
      */
     public function favoriteOutputDefinitionsTableProxyAction(Request $request)
     {
+        $this->checkPermission('web2print_web2print_favourite_output_channels');
+        
         if ($request->request->getString('data')) {
             $data = json_decode($request->request->getString('data'), true);
             if ($request->query->getString('xaction') === 'destroy') {
@@ -138,7 +140,6 @@ class AdminController extends UserAwareController
      */
     public function saveOrUpdateFavoriteOutputDefinitionAction(Request $request)
     {
-
         $configuration = $request->request->getString('configuration');
         $id = $request->request->getInt('existing');
         $newName = strip_tags($request->request->getString('text'));
